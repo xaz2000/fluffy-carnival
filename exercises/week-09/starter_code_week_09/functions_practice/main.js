1ed5c80c42f741957839704a3a78587300e42213
xaz2000 <144857487+xaz2000@users.noreply.github.com>
2023-10-31 21:45:17 -0400

    Summary

---
+++ exercises/week-09/starter_code_week_09/functions_practice/main.js
@@ -0,0 +1,25 @@
+/*
+    Write a function for each of the following problems:
+
+    [1] Console logs "hello"
+    [2] Console logs "world"
+    [3] Takes a parameter called "name" and console logs it
+    [4] Takes a parameter called "name" and console logs "hello" + name
+    [5] Takes a parameter called "number" and multiplies it by itself, then console logs the result
+    [6] Take a number as a parameter and return itself squared
+    [7] Have 3 optional parameters, all numbers. Add all the numbers together. If the function is called without passing any numbers in, simply return 10
+*/
+
+function sayHello(){
+    console.log('Hello')
+}
+sayHello()
+
+function callName(name, city){
+    // console.log("Hello" + name)
+    console.log(`Hello, my name is ${name} and I am from ${city}`)
+}
+
+callName("Kevin", "Los Angeles")
+// callName("Chris")
+// callName("Carmen")
